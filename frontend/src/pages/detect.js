bd47789a4358f33e2e947d0ac5ac905c231fe5da
Max Hua <70117371+BleakBubbles@users.noreply.github.com>
2024-08-03 22:05:19 -0400

    Update detect.js

--- frontend/src/pages/detect.js
+++ frontend/src/pages/detect.js
@@ -10,28 +10,23 @@ export default function Detect() {
 
     return (
         <div
-            className="bg-slate-100 w-full h-screen flex flex-col items-center"
+            className="bg-gradient-to-b from-black to-cyan-900 w-full h-screen flex flex-col items-center"
             onPaste={(e) => {
                 if (e.clipboardData.files.length) {
                     setImage(e.clipboardData.files[0])
                 }
             }}>
             <h1
-                className="text-white mt-5 text-4xl"
+                className="text-white mt-10 mb-5 text-8xl"
             >Was this AI Art?</h1>
-            <p className="text-white">Upload the image here and have our bot tell you.</p>
+            <p className="text-white mb-5">Upload the image here and have our bot tell you.</p>
             <div
-                className={
-                    image
-                    ? "bg-slate-100 rounded m-5 flex flex-col justify-items-center items-center border-solid border-2 border-slate-400" 
-                    : "bg-slate-100 rounded m-5 flex flex-col justify-items-center items-center border-solid border-2 border-slate-400 cursor-pointer"
-                }
-                onClick={() => hiddenInput.current.click()}
-            >
+                className="text-slate-900 bg-slate-200 m-10 p-5 rounded-2xl border-cyan-900 border-[2rem] m-5 w-2/4 h-2/4 flex flex-col justify-items-center items-center cursor-pointer hover:scale-110"
+                onClick={() => hiddenInput.current.click()}>
                 {image ? (<img alt="" className="relative w-full h-full object-contain" src={URL.createObjectURL(image)}></img>)
                 : ( <div className="relative flex flex-col justify-items-center items-center">
-                        <img src={arrow} className="size-20"></img>
-                        <p>Paste or Upload Image</p>
+                        <img src={arrow} className="size-50 m-5 hover:scale-110"></img>
+                        <h1 className="text-4xl">Paste or Upload Image</h1>
                     </div>)}
             </div>
             <input
@@ -45,22 +40,13 @@ export default function Detect() {
                 }}
             >
             </input>
-            <div className="flex flex-row justify-items-center items-center">
-                <div
-                    disabled={image == null}
-                    class={image ? "basis-1/2 text-white bg-blue-400 p-3 mr-3 rounded hover:cursor-pointer hover:bg-blue-500" :
-                        "basis-1/2 text-white bg-blue-400 p-3 mr-3 rounded"
-                    }
-                    onClick={() => setImage(null)}
-                >Detect</div>
-                <div
-                    disabled={image == null}
-                    class={image ? "basis-1/2 text-white bg-red-400 p-3 ml-3 rounded hover:cursor-pointer hover:bg-red-500" :
-                        "basis-1/2 text-white bg-red-400 p-3 ml-3 rounded"
-                    }
-                    onClick={() => setImage(null)}
-                >Remove</div>
-            </div>
+            <div
+                disabled={image == null}
+                class={image ? "m-5 text-black text-5xl w-1/4 h-20 bg-blue-300 p-3 rounded hover:cursor-pointer hover:scale-110" :
+                    "m-5 text-black text-5xl w-1/4 h-20 bg-blue-300 p-3 mr-3 rounded"
+                }
+                onClick={() => setImage(null)}
+            >Check Origin</div>
         </div>
     )
 }
